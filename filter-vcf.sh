723bd326a3792b6f525306ddbd875c0db8affe40
SteffensonLab <spann057@umn.edu>
2024-10-15 12:05:40 -0500

    Update filter-vcf.sh

--- filter-vcf.sh
+++ filter-vcf.sh
@@ -10,9 +10,9 @@ vcftools --vcf $nohetsvcf --max-missing 0.9 --recode > $outputvcf_nomissing
 #use vcftools to filter for minor allele frequency, average min read depth and bi-allelic sites - in this case 3% threshold and minimum average read depth of 10 for a site
 outputvcf_maf="/path/to/filtered-maf-mindp-vcf/"
 vcftools --vcf $outputvcf_nomissing --maf 0.03 --min-meanDP 10 --min-alleles 2 --max-alleles 2 --recode > $outputvcf_maf
-#filter by LD - sliding window of 50kb, LD=0.2
+#filter by LD - sliding window of 5 sites, LD=0.2
 bcf="/path/to/filtered-LD-bcf/"
-bcftools +prune -l 0.2 -w 50 $outputvcf_maf -Ob -o $bcf
+bcftools +prune -l 0.2 -w 5 $outputvcf_maf -Ob -o $bcf
 bcftools view $bcf > $vcf
 #count number of SNPs
 vcf="/path/to/vcf"
