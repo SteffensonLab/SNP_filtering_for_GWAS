7051c98b91bd6b4fc6d4978a3a725f7f9ff0e78a
SteffensonLab <spann057@umn.edu>
2024-10-14 21:33:22 -0500

    Create filter-vcf.sh

---
+++ filter-vcf.sh
@@ -0,0 +1,19 @@
+#filtering vcf - pipeline for inbred diploid species - WBDC
+#requires vcftools v0.1.16 - https://github.com/vcftools/vcftools 
+#and bcftools https://samtools.github.io/bcftools/bcftools.html
+
+#first convert heterozygote calls to missing- use github.com/bspan07/filter-vcf/convert_hets_to_missing_vcf.sh
+#use vcftools to filter missing data
+nohetsvcf="/path/to/no-hets-vcf/"
+outputvcf_nomissing="/path/to/no-missing/"
+vcftools --vcf $nohetsvcf --max-missing 0.9 --recode > $outputvcf_nomissing  
+#use vcftools to filter for minor allele frequency, average min read depth and bi-allelic sites - in this case 3% threshold and minimum average read depth of 10 for a site
+outputvcf_maf="/path/to/filtered-maf-mindp-vcf/"
+vcftools --vcf $outputvcf_nomissing --maf 0.03 --min-meanDP 10 --min-alleles 2 --max-alleles 2 --recode > $outputvcf_maf
+#filter by LD - sliding window of 50kb, LD=0.2
+bcf="/path/to/filtered-LD-bcf/"
+bcftools +prune -l 0.2 -w 50 $outputvcf_maf -Ob -o $bcf
+bcftools view $bcf > $vcf
+#count number of SNPs
+vcf="/path/to/vcf"
+bcftools stats $vcf > vcf_statistics.txt
